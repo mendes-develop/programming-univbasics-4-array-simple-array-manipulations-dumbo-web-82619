32be33a97b2bb4709564a5503767fb2add5d3044
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 13:26:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,10 +35,15 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
   
 end
 
-def using_delete_at ()
+def using_delete_at (array, interger)
   
 end
 
