3f6e510b11599bb123940bd43f4ec9aa4b42ef1d
Alex Mendes <mendes.develop@gmail.com>
2019-08-19 13:10:00 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,44 @@
+def using_push ()
+  
+end
+
+def using_unshift()
+  
+end
+
+def using_pop()
+  
+end
+
+def pop_with_args()
+  
+end
+
+def using_unshift
+  
+end
+
+def shift_with_args()
+  
+end
+
+def using_concat ()
+  
+end 
+
+def using_insert()
+  
+end
+
+def using_uniq()
+  
+end
+
+def using_flatten()
+  
+end
+
+def using_delete_at ()
+  
+end
+
